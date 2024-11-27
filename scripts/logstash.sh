dcfa0b67d6198be79ef16bb3919e42d3e2ed3403
noeliasd03 <145052078+noeliasd03@users.noreply.github.com>
2024-11-27 19:04:32 +0100

    Update logstash.sh

--- scripts/logstash.sh
+++ scripts/logstash.sh
@@ -4,8 +4,6 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y openjdk-11-jre
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-sudo sh -c 'echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" > /etc/apt/sources.list.d/elastic-7.x.list'
 sudo apt update
 sudo apt install -y logstash
 
